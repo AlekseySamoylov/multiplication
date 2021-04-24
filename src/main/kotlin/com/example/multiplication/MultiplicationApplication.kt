e7882d13e2187344f9227ccdc9473846fee89e58
alekseysamoylov <alekseysamoylov89@gmail.com>
2021-04-24 20:06:14 +0400

    initial

---
+++ src/main/kotlin/com/example/multiplication/MultiplicationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.multiplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiplicationApplication
+
+fun main(args: Array<String>) {
+  runApplication<MultiplicationApplication>(*args)
+}
