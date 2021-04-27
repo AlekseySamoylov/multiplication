f0e9ce0cfc4fbb8310639cccd99a1ec46f8ab4e7
alekseysamoylov <alekseysamoylov89@gmail.com>
2021-04-27 23:53:35 +0400

    add static content

--- src/main/kotlin/com/example/multiplication/WebConfiguration.kt
+++ src/main/kotlin/com/example/multiplication/WebConfiguration.kt
@@ -2,6 +2,7 @@ package com.example.multiplication
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,5 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfiguration : WebMvcConfigurer {
   override fun addCorsMappings(registry: CorsRegistry) {
     registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+  }
+
+  override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+    registry.addResourceHandler("/static/*").addResourceLocations("/", "classpath:/static/")
   }
 }
